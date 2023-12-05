c664e2592355a0c1f8d86af5be025fbe051181f5
Lucifer <38715155+jonesroot@users.noreply.github.com>
2023-12-05 22:16:14 +0700

    update

--- Key/__init__.py
+++ Key/__init__.py
@@ -22,9 +22,9 @@ from ptbcontrib.postgres_persistence import PostgresPersistence
 
 StartTime = time.time()
 
-def get_user_list(__init__, key):
-    with open(f"{os.getcwd()}/Key/{__init__}", "r") as json_file:
-        return json.load(json_file)[key]
+#def get_user_list(__init__, key):
+#    with open(f"{os.getcwd()}/Key/{__init__}", "r") as json_file:
+#        return json.load(json_file)[key]
 
 # enable logging
 FORMAT = "[Key] %(message)s"
